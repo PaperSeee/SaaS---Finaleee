2ab199e1611500c2776c53f89b6a75da28038503
paperseee <ilias300@outlook.be>
2025-05-12 02:42:15 +0200

    feat: Add ReviewsLayout component to render children in reviews page

---
+++ app/company/[companyId]/reviews/layout.tsx
@@ -0,0 +1,10 @@
+// app/company/[companyId]/reviews/layout.tsx
+import { ReactNode } from "react";
+
+export default function ReviewsLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <>{children}</>;
+}
