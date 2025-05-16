4c0222e93f8ee0fbfadbb931fece6ab823755862
paperseee <ilias300@outlook.be>
2025-05-16 03:03:12 +0200

    refactor: add type definition for ref in Home component for improved type safety

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   const router = useRouter();
 
   // For parallax scrolling effect
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"]
